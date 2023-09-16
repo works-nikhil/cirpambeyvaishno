77496c84591928ed8dcff65b79439980ef548e76
Nikhil Bhojwani <107989387+nikhil-apprely@users.noreply.github.com>
2023-09-16 20:09:19 +0530

    form G

--- app/(auth)/insolvencyprocess/page.tsx
+++ app/(auth)/insolvencyprocess/page.tsx
@@ -238,6 +238,21 @@ const InsolvencyProcess = () => {
                   </a>
                 </div>
               </div>
+
+              <div className="flex items-center mb-2">
+                <div className="flex-1">
+                  <p className="mr-4">16. Form G 3rd Issue</p>
+                </div>
+                <div>
+                  <a href="/pdfs/Form G 3rd for IBBi.pdf" download>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
+                      Download File
+                    </button>
+                  </a>
+                </div>
+              </div>
+
+
             </div>
           </div>
         </div>
